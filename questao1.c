d40ca02a65c0d59853ca2868f5f504da3e87f693
augustguim <lagg.eng19@uea.edu.br>
2022-08-17 01:38:32 -0400

    questao 1

---
+++ questao1.c
@@ -0,0 +1,30 @@
+/*
+	lazaro Goes	1915120087
+*/
+
+#include<stdio.h>
+
+int main(int argc, char**argv)
+{
+
+	int i, num, maior;
+
+	maior=0;
+	i=1;
+	do
+	{
+		printf("Digite o %d°número: ",i++);
+		scanf("%d", &num);
+
+		if(num>maior)
+		{
+			maior=num;
+		}
+	}
+	while(i<=5);
+
+	printf("o maior numero é: %d\n", maior);
+
+	return 0;
+
+}
